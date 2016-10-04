ef9b0db506c6c38aa345521ed90954497793c943
David Thorpe <david@thorpe>
2016-10-04 21:00:28 +0100

    Add formattedRole and formattedName

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,22 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+// var firstName = "David";
+
+// var age = 33;
+
+// console.log(firstName);
+
+// var awesomeThoughts = "I am David Thorpe and I am AWESOME";
+
+// console.log(awesomeThoughts);
+
+// var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+
+// $("#main").append(funThoughts);
+
+var name = "David Thorpe";
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = "Web Developer";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
