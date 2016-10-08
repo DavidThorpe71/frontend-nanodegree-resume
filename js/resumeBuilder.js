5f3112d4b97b0728526ef28d489afd2e27d708ed
David Thorpe <david@thorpe>
2016-10-08 14:47:35 +0100

    Add blank slate for work, projects, bio and education categories

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,87 +1,105 @@
-var name = "David Thorpe";
+var work = {
+	"jobs": [
+		{
+		"employer":,
+		"title":,
+		"location":,
+		"dates":,
+		"description":,
+		},
+		{
+		"employer":,
+		"title":,
+		"location":,
+		"dates":,
+		"description":,
+		},
+		{
+		"employer":,
+		"title":,
+		"location":,
+		"dates":,
+		"description":,
+		},
+	]
+};
 
-var role = "Web Developer";
+
+var projects = {
+	"projects": [
+		{
+		"title":,
+		"dates":,
+		"description":,
+		"images": ["url", "url", "url"],
+		}
+		{
+		"title":,
+		"dates":,
+		"description":,
+		"images": ["url", "url", "url"],
+		}
+		{
+		"title":,
+		"dates":,
+		"description":,
+		"images": ["url", "url", "url"],
+		}
+	]
+
+};
+
 
 var bio = {
 	"name": "David Thorpe",
 	"role": "Web Developer",
+	"welcomeMessage": "Hello, I am a budding web developer looking for work!",
+	"bioPic": "images/fry.jpg"
 	"contacts": {
 		"mobile": "07880878177",
 		"email": "vegas_71@yahoo.co.uk",
 		"github": "DavidThorpe71",
 		"twitter": "@davisthorpe",
-	},
-	
-	"welcomeMessage": "Hello, I am a budding web developer looking for work!",
-	"skills": [
-	"awesomeness", "programming", "learning", "JS"
-	],
-	"bioPic": "images/fry.jpg"
+		"location": "London"
+		},
+	"skills": ["awesomeness", "programming", "learning", "JS"],
 };
 
-
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-$("#header").prepend(formattedRole);
-
-var formattedName = HTMLheaderName.replace("%data%", name);
-$("#header").prepend(formattedName);
-
-var formattedbioPic = HTMLbioPic.replace("%data%", bio.bioPic);
-$("#header").append(formattedbioPic);
-
-var formattedwelcomeMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
-$("#header").append(formattedwelcomeMessage);
-
-
-var formattedmobile = HTMLmobile.replace("%data%",bio.contacts.mobile)
-$("#topContacts").append(formattedmobile);
-var formattedemail = HTMLemail.replace("%data%",bio.contacts.email)
-$("#topContacts").append(formattedemail);
-var formattedgithub = HTMLgithub.replace("%data%",bio.contacts.github)
-$("#topContacts").append(formattedgithub);
-var formattedtwitter = HTMLtwitter.replace("%data%",bio.contacts.twitter)
-$("#topContacts").append(formattedtwitter);
-
-
-$("#header").append(HTMLskillsStart);
-var formattedskills = HTMLskills.replace("%data%", bio.skills[0])
-$("#header").append(formattedskills);
-var formattedskills = HTMLskills.replace("%data%", bio.skills[1])
-$("#header").append(formattedskills);
-var formattedskills = HTMLskills.replace("%data%", bio.skills[2])
-$("#header").append(formattedskills);
-var formattedskills = HTMLskills.replace("%data%", bio.skills[3])
-$("#header").append(formattedskills);
-
-
-var work = {};
-
-work.title = "Senior Administrator";
-work.employer = "NHS Camden CCG";
-work.dates = "2013-Current";
-work.location = "London";
 
 var education = {
 	"schools": [
 		{
 		"name": "Maiden Erlegh School",
-		"city": "London",
-		"level": "A-Level",
-		"courses": ["Maths", "English", "Sociology"]
+		"location": "Reading",
+		"degree dates": "A-Level",
+		"url": "",
+		"majors": ["Maths", "English", "Sociology"]
+		},
+		{
+		"name": "Maiden Erlegh School",
+		"location": "Reading",
+		"degree dates": "A-Level",
+		"url": "",
+		"majors": ["Maths", "English", "Sociology"]
+		}
+	],
+	"onlineCourses": [
+		{
+		"title": "",
+		"school": "",
+		"dates": "",
+		"url": ""
+		},
+		{
+		"title": "",
+		"school": "",
+		"dates": "",
+		"url": ""
+		},{
+		"title": "",
+		"school": "",
+		"dates": "",
+		"url": ""
 		}
 	]
 };
-
-education["name"] = "Maiden Erlegh School";
-education["yearsattended"] = "1996-2001";
-education["city"] = "Reading";
-
-
-$("#workExperience").append(HTMLworkStart);
-var formattedTitle = HTMLworkTitle.replace("%data%", work.title);
-$("#workExperience").append(formattedTitle);
-
-$("#education").append(HTMLschoolStart);
-var formattedName = HTMLschoolName.replace("%data%", education["name"]);
-$("#education").append(formattedName);
-// $("#education").append(education.name);
